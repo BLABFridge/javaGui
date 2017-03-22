18c3aae1d8c0dda40b662bf144c533c84fe5b8c5
brownliam <liam.a.brown@live.ca>
2017-03-22 19:49:59 -0400

    Update ButtonListener.java

--- ButtonListener.java
+++ ButtonListener.java
@@ -20,12 +20,14 @@ public class ButtonListener implements ActionListener{
                 System.out.print("Get Expiring pressed");
                 gui.editText1 = gui.expiringTextArea.getText().toString();
                 int dayInput = Integer.valueOf(gui.editText1);
+                gui.expiringTextArea.setText(gui.defaultExpiringAreaText);
                 gui.getItemsExpiringBefore(dayInput);
                 break;
             case "Set Timeout":
                 System.out.print("Set Timeout pressed");
                 gui.editText1 = gui.setTimeoutTextArea.getText().toString();
                 int timeoutInput = Integer.valueOf(gui.editText1);
+                gui.setTimeoutTextArea.setText(gui.defaultTimeoutAreaText);
                 //method call to set the timeout.
             default: System.out.print("default case");
            }
