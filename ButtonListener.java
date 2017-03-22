5127b65d33d9b11ca111dac7d5d8483ae5b16455
brownliam <liam.a.brown@live.ca>
2017-03-22 19:35:19 -0400

    Update ButtonListener.java
    Added partial functionality for buttons

--- ButtonListener.java
+++ ButtonListener.java
@@ -2,15 +2,34 @@ import javax.swing.JButton;
 import java.awt.event.*;
 
 public class ButtonListener implements ActionListener{
-	
-	private GUIClass gui;
+    
+    private GUIClass gui;
 
-	public ButtonListener(GUIClass gui) {
-		this.gui = gui;
-	}
+    public ButtonListener(GUIClass gui) {
+        this.gui = gui;
+    }
 
-	public void actionPerformed(ActionEvent e) {
-		gui.getItemsExpiringBefore(0);
-	}
+    public void actionPerformed(ActionEvent e) {
+        String source = (String) e.getActionCommand();
+        switch(source){
+            case "Get List": 
+                System.out.print("Get List pressed");
+                gui.getItemsExpiringBefore(0);
+                break;
+            case "Get Expiring": 
+                System.out.print("Get Expiring pressed");
+                gui.editText1 = gui.expiringTextArea.getText().toString();
+                int dayInput = Integer.valueOf(gui.editText1);
+                gui.getItemsExpiringBefore(dayInput);
+                break;
+            case "Set Timeout":
+                System.out.print("Set Timeout pressed");
+                gui.editText1 = gui.setTimeoutTextArea.getText().toString();
+                int timeoutInput = Integer.valueOf(gui.editText1);
+                //method call to set the timeout.
+            default: System.out.print("default case");
+           }
+
+    }
 
 }
